9cd0c75500b3b5cb1f57205614893f2cd089ef78
gitpold <oleo97@web.de>
2018-04-10 11:57:00 +0000

    started doing magic and some minor changes

--- compiler/src/typecheck.h
+++ compiler/src/typecheck.h
@@ -10,4 +10,4 @@ int checkVoid(int line, int col, int type);
 int checkVar(int line, int col, char *name);
 int checkFunc(int line, int col, char *name);
 
-extern expression_t* checkAssignment(int line, int col, expression_t *exp1, expression_t *exp2);
+expression_t* checkAssignment(int line, int col, expression_t *exp1, expression_t *exp2);
